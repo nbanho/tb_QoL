b1b9daca544bfa39948669b60f2bcd2a0060e3bc
nbanho <nicbanjo@gmail.com>
2025-02-06 09:25:08 +0100

    revised analysis with new data

--- preprocessing/prep.R
+++ preprocessing/prep.R
@@ -107,6 +107,7 @@ df_prep <- df %>%
     hiv_cd4_mm3_nr = ifelse(hiv_cd4_mm3_nr > 1500, NA, hiv_cd4_mm3_nr),
     hiv_viral_load = ifelse(hiv_viral_load_nr < 0, 0, hiv_viral_load),
     hiv_viral_load_nr = ifelse(hiv_viral_load_nr == -1, NA, hiv_viral_load_nr),
+    hiv_viral_load_nr = if_else(hiv_viral_load_nr == 0, NA, hiv_viral_load_nr),
     mdr = ifelse(mb_xpert_t1_rifresist == 1, 1, ifelse(mb_drug_rif == 2, 1, 0)),
     mdr = ifelse(is.na(mdr), 0, mdr),
     highbact = ifelse(mb_smear1_result_who %in% c(3, 4), 1, 0),
